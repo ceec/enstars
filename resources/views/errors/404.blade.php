1bfb49c17419a19616b5b77f7ec04240cfe3643b
Christine Stone <cc@battab.com>
2016-10-13 23:37:40 -0600

    added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.layout')
+
+@section('title')
+@parent
+Enstars Info
+@stop
+
+@section('content')
+
+<div class="container">
+    <h1>404: Page not found!</h1>
+    <div class="row">
+
+
+    </div>
+</div>
+@endsection
