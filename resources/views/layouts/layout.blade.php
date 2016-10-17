f42090395fa41b91001470a4c3f79233a85b3a00
Christine Stone <cc@battab.com>
2016-10-17 09:25:41 -0600

    updated boy.css and natsume font

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -13,6 +13,7 @@
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Loved+by+the+King" rel="stylesheet"> 
 
     <!-- Styles -->
     <link href="/css/bootstrapyeti.min.css" rel="stylesheet">
