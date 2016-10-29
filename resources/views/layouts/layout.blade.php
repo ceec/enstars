9591457b6e332265efe332ec35631fb40cf0aaab
Christine Stone <cc@battab.com>
2016-10-29 00:35:05 -0600

    cheated and hardcoded current event

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -59,7 +59,7 @@
                         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Events <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Current Event</a></li>
+            <li><a href="/event/magical-halloween">Current Event</a></li>
             <li><a href="#">Stories</a></li>
             <li><a href="#">Calculator</a></li>
           </ul>
