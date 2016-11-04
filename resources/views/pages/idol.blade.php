eb37fa81d396a172fdc376f1e7310ee8e6760264
Christine Stone <cc@battab.com>
2016-11-03 23:53:49 -0600

    flip name orientation so uniform

--- resources/views/pages/idol.blade.php
+++ resources/views/pages/idol.blade.php
@@ -10,7 +10,7 @@
     <div class="row">
         <div class="col-md-12">
 
-        	<h1>{{$boy->english_name}}</h1>
+        	<h1>{{$boy->first_name}} {{$boy->last_name}}</h1>
                 <div class="row">
                     <?php $x=1; ?>
                     @foreach($cards as $card)
