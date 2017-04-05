1fac369f3c660a845d4ca569d99d459cb29216bb
Christine Stone <cc@battab.com>
2017-04-05 01:45:01 -0600

    making event and scout urls dynamic

--- app/Event.php
+++ app/Event.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    /**
+     * Get the current event 
+     */
+    public static function current()
+    {
+          $current = Event::where('active','=','1')->first();
+
+			return $current->url;      
+    }
 }
--- app/Scout.php
+++ app/Scout.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Scout extends Model
 {
-    //
+    
+    /**
+     * Get the current scout (main kind not story)
+     */
+    public static function current()
+    {
+          $current = Scout::where('active','=','1')->where('type_id','=',1)->first();
+
+			return $current->url;      
+    }
+
+
+
+
 }
