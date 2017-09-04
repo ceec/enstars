ea9a3007664a6532a371338c64e08db9adb2c5a8
Christine Stone <cc@battab.com>
2017-09-03 20:23:49 -0600

    reformat store page

--- resources/views/pages/store.blade.php
+++ resources/views/pages/store.blade.php
@@ -12,114 +12,77 @@ Store | enstars.info
 
 		    <h1>Store</h1>
 
-        <p>The items sold here go towards keeping enstars.info running! We also provide box split services, if anyone has an item they are interested in please message us using the contact form.</p>
+        <p>The items sold here go towards keeping enstars.info running!</p>
         <p>All items are shipped from Colorado using USPS.</p>
 
-		    <div id='collection-component-f9457d5836a'></div>
-
-<script type="text/javascript">
-/*<![CDATA[*/
-
-(function () {
-  var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
-  if (window.ShopifyBuy) {
-    if (window.ShopifyBuy.UI) {
-      ShopifyBuyInit();
-    } else {
-      loadScript();
-    }
-  } else {
-    loadScript();
-  }
-
-  function loadScript() {
-    var script = document.createElement('script');
-    script.async = true;
-    script.src = scriptURL;
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
-    script.onload = ShopifyBuyInit;
-  }
-
-  function ShopifyBuyInit() {
-    var client = ShopifyBuy.buildClient({
-      domain: 'enstars.myshopify.com',
-      apiKey: 'd76a665dfe453bc4fd4143fcf012476e',
-      appId: '6',
-    });
-
-    ShopifyBuy.UI.onReady(client).then(function (ui) {
-      ui.createComponent('collection', {
-        id: 413162062,
-        node: document.getElementById('collection-component-f9457d5836a'),
-        moneyFormat: '%24%7B%7Bamount%7D%7D',
-        options: {
-  "product": {
-    "variantId": "all",
-    "contents": {
-      "imgWithCarousel": false,
-      "variantTitle": false,
-      "description": false,
-      "buttonWithQuantity": false,
-      "quantity": false
-    },
-    "styles": {
-      "product": {
-        "@media (min-width: 601px)": {
-          "max-width": "calc(25% - 20px)",
-          "margin-left": "20px",
-          "margin-bottom": "50px"
-        }
-      }
-    }
-  },
-  "cart": {
-    "contents": {
-      "button": true
-    },
-    "styles": {
-      "footer": {
-        "background-color": "#ffffff"
-      }
-    }
-  },
-  "modalProduct": {
-    "contents": {
-      "img": false,
-      "imgWithCarousel": true,
-      "variantTitle": false,
-      "buttonWithQuantity": true,
-      "button": false,
-      "quantity": false
-    },
-    "styles": {
-      "product": {
-        "@media (min-width: 601px)": {
-          "max-width": "100%",
-          "margin-left": "0px",
-          "margin-bottom": "0px"
-        }
-      }
-    }
-  },
-  "productSet": {
-    "styles": {
-      "products": {
-        "@media (min-width: 601px)": {
-          "margin-left": "-20px"
-        }
-      }
-    }
-  }
-}
-      });
-    });
-  }
-})();
-/*]]>*/
-</script>
-                <hr>
-
+        <h2>Character Magnets</h2>
+        <p>
+          These are magnets from an Enstars Kuji. They come with the magnet and the box.<br> They are $10 each + shipping. Shipping has been ~$3 in the US and ~$14 international.<br>
+          If you want one <a href="/contact">send me a message with the contact form!</a><br>
+          <a href="/news/character-magnets-available-for-purchase">See how we got them here!</a>
+        </p>
+        <h3>Magnet Box</h3>
+        <img src="/images/store/1.JPG">
+        <h3>Magnets Available</h3>
+        <div class="row">
+          <div class="col-md-4">
+          <img src="/images/2.JPG">
+          <h5>Subaru</h5>
+          </div>
+          <div class="col-md-4">
+            <img src="/images/3.JPG">
+            <h5>Hokuto</h5>
+          </div>
+          <div class="col-md-4">
+             <img src="/images/4.JPG"> 
+             <h5>Makoto</h5>
+          </div>                    
         </div>
+        <div class="row">
+          <div class="col-md-4">
+          <img src="/images/8.JPG">
+          <h5>Kaoru</h5>
+          </div>
+          <div class="col-md-4">
+            <img src="/images/9.JPG">
+            <h5>Adonis</h5>
+          </div>
+          <div class="col-md-4">
+             <img src="/images/14.JPG"> 
+             <h5>Arashi</h5>
+          </div>                    
+        </div>           
+        <div class="row">
+          <div class="col-md-4">
+          <img src="/images/16.JPG">
+          <h5>Izumi</h5>
+          </div>
+          <div class="col-md-4">
+            <img src="/images/20.JPG">
+            <h5>Yuzuru</h5>
+          </div>
+          <div class="col-md-4">
+             <img src="/images/21.JPG"> 
+             <h5>Eichi</h5>
+          </div>                    
+        </div>   
+        <div class="row">
+          <div class="col-md-4">
+          <img src="/images/24.JPG">
+          <h5>Keito</h5>
+          </div>
+          <div class="col-md-4">
+            <img src="/images/26.JPG">
+            <h5>Yuta</h5>
+          </div>
+          <div class="col-md-4">
+             <img src="/images/27.JPG"> 
+             <h5>Hinata</h5>
+          </div>                    
+        </div>           
+
+
+
 
     </div>
 </div>
