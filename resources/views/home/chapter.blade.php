a6f3c22be2eac93d15a2244653bad256eaffde86
Christine Stone <cc@battab.com>
2017-09-24 20:22:53 -0600

    add buttons to translation UI to add boy class

--- resources/views/home/chapter.blade.php
+++ resources/views/home/chapter.blade.php
@@ -33,28 +33,99 @@
     	</div>
     	<div class="col-md-6">
 			
-			{!! Form::textarea('text_j',$slide->text_j,['class'=>'form-control ajaxTest','id'=>$slide->id, 'rows' => 3, 'cols' => 100, 'placeholder'=>'Japanese Text']) !!} <br>
-			{!! Form::textarea('text_e', $slide->text_e,['class'=>'form-control ajaxTest','id'=>$slide->id, 'rows' => 3, 'cols' => 100, 'placeholder'=>'English Text']) !!} <br>
-			{!! Form::textarea('notes', $slide->notes,['class'=>'form-control ajaxTest', 'id'=>$slide->id, 'rows' => 3, 'cols' => 100, 'placeholder'=>'Notes']) !!} <br>
+			{!! Form::textarea('text_j',$slide->text_j,['class'=>'form-control ajaxTest','id'=>'j-'.$slide->id, 'rows' => 3, 'cols' => 100, 'placeholder'=>'Japanese Text']) !!} <br>
+			{!! Form::textarea('text_e', $slide->text_e,['class'=>'form-control ajaxTest','id'=>'e-'.$slide->id, 'rows' => 3, 'cols' => 100, 'placeholder'=>'English Text']) !!} 
+            @foreach($boys as $key => $boy)
+                <button class="add-boy-css" data-class="{{$boy}}" data-slide="{{$slide->id}}">{{$boy}}</button> 
+            @endforeach
+            <br>
+			{!! Form::textarea('notes', $slide->notes,['class'=>'form-control ajaxTest', 'id'=>'n-'.$slide->id, 'rows' => 3, 'cols' => 100, 'placeholder'=>'Notes']) !!} <br>
 			{!! Form::hidden('slide_id', $slide->id) !!}
 			{!! Form::hidden('chapter_id', $chapter->id) !!}
 			{!! Form::hidden('story_id', $story->id) !!}
-			{!! Form::select('boy_id', $boys,$slide->boy_id, ['class'=>'dropdownAjax','id'=>$slide->id, 'placeholder' => 'Speaker']) !!}   {{$slide->slide}} | Last updated: <span id="lastupdated-{{$slide->id}}">{{$slide->updated_at}}</span>
+			{!! Form::select('boy_id', $boys,$slide->boy_id, ['class'=>'dropdownAjax','id'=>'s-'.$slide->id, 'placeholder' => 'Speaker']) !!}   {{$slide->slide}} | Last updated: <span id="lastupdated-{{$slide->id}}">{{$slide->updated_at}}</span>
 			
     	</div>
    	</div>
    	<br><br>
     @endforeach
 </div>
-<script>
-	//lets do some ajax woop
+            <script>
 
-	//should I use $ for jQuery SEEMS SO WEIRD
+            //adding in class names to textarea with button press - Christine -2017-09-24
+            function getInputSelection(el) {
+    var start = 0, end = 0, normalizedValue, range,
+        textInputRange, len, endRange;
 
-	//jQuery('body').on('click','')
+    if (typeof el.selectionStart == "number" && typeof el.selectionEnd == "number") {
+        start = el.selectionStart;
+        end = el.selectionEnd;
+    } else {
+        range = document.selection.createRange();
+
+        if (range && range.parentElement() == el) {
+            len = el.value.length;
+            normalizedValue = el.value.replace(/\r\n/g, "\n");
+
+            // Create a working TextRange that lives only in the input
+            textInputRange = el.createTextRange();
+            textInputRange.moveToBookmark(range.getBookmark());
+
+            // Check if the start and end of the selection are at the very end
+            // of the input, since moveStart/moveEnd doesn't return what we want
+            // in those cases
+            endRange = el.createTextRange();
+            endRange.collapse(false);
+
+            if (textInputRange.compareEndPoints("StartToEnd", endRange) > -1) {
+                start = end = len;
+            } else {
+                start = -textInputRange.moveStart("character", -len);
+                start += normalizedValue.slice(0, start).split("\n").length - 1;
+
+                if (textInputRange.compareEndPoints("EndToEnd", endRange) > -1) {
+                    end = len;
+                } else {
+                    end = -textInputRange.moveEnd("character", -len);
+                    end += normalizedValue.slice(0, end).split("\n").length - 1;
+                }
+            }
+        }
+    }
+
+    return {
+        start: start,
+        end: end
+    };
+}
+
+function replaceSelectedText(el, text) {
+    var sel = getInputSelection(el), val = el.value;
+    el.value = val.slice(0, sel.start) + text + val.slice(sel.end);
+}
 
 
+$('.add-boy-css').on('click',function(){
+    var text = window.getSelection().toString();
+    
+    //this would have to be the closest text area.
+    var slideID = $(this).data("slide");
+    var className = $(this).data("class");
+    
+    //get selected text
+    var text = window.getSelection().toString();
 
+    //wrap selected text in a class with boy's name <span class="Tori">text</span>
+    var el = document.getElementById("e-"+slideID);
+    replaceSelectedText(el, '<span class="'+className+'">'+text+'</span>');
+
+    //save added classes 
+    var slideName = 'text_e';
+    var slideValue = $('#e-'+slideID).val();
+
+    slideContentAjax(slideID,slideName,slideValue);
+
+});
 
 
 
@@ -106,31 +177,15 @@ $('.name_e').keyup(function(test) {
 
 //http://stackoverflow.com/questions/28051963/jquery-keypress-function-with-multiple-input-with-same-id
 
-$('.ajaxTest').keyup(function(test) {
-	var $self = $(this);
-	//make this global to get into the delay function
-	slideValue = $self.val();
-	slideID = $self.attr('id');
-	slideName = $self.attr('name');
 
-	//do i need to pass through the type? or just use it as a variable in PHP
-
-  delay(function(){
-  	// $.post('/add/translationajax',{slide_id:slideID,name:slideName,value:slideValue},function(data) {
-  	// 	//on success dooo
-  	// 	console.log('posted!');
-  	// 	console.log(data);
-  	// });
-
-
-
-    $.ajaxSetup({
+//saving slide content
+function slideContentAjax(slideID,slideName,slideValue) {
+        $.ajaxSetup({
             headers: {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             }
         })
 
-        //e.preventDefault(); 
         $.ajax({
 
             type: "POST",
@@ -138,32 +193,49 @@ $('.ajaxTest').keyup(function(test) {
             data: {slide_id:slideID,name:slideName,value:slideValue},
             dataType: 'json',
             success: function (data) {
-            	//update the timestamp
-            	$('#lastupdated-'+slideID).html(data.date);
+                //update the timestamp
+                $('#lastupdated-'+slideID).html(data.date);
                 //console.log(data);
             },
             error: function (data) {
                 //console.log('Error:', data);
             }
         });
+}
 
 
 
+//save the content of the textareas on keyup
+$('.ajaxTest').keyup(function(test) {
+	var $self = $(this);
+	//make this global to get into the delay function
+	slideValue = $self.val();
+	slideID = $self.attr('id');
 
-    //console.log(slideValue);
-    //console.log(slideID);
-    //console.log(slideName);
+    //remove the first two characters - Christine 2017-09-24
+    slideID = slideID.slice(2);
+
+	slideName = $self.attr('name');
+
+  //wait one second
+  delay(function(){
+    //save the content
+    slideContentAjax(slideID,slideName,slideValue);
   }, 1000 );
 });
 
-///submmit the speaker box
+
+
+
+///submit the speaker box
     $('.dropdownAjax').change(function() {
 		var $self = $(this);
 		//make this global to get into the delay function
 		slideSpeaker = $self.val();
 		slideID = $self.attr('id');
 		slideName = $self.attr('name');
-
+        //remove the first two characters - Christine 2017-09-24
+        slideID = slideID.slice(2);
 
     $.ajaxSetup({
             headers: {
