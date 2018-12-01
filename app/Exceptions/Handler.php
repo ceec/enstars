854d024a29b714d1be6b270ea24c3dfdeeb86c32
Christine Stone <cc@battab.com>
2018-11-30 21:07:20 -0700

    add rest of error emails

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,6 +6,12 @@ use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
+
+use Mail;
+use Symfony\Component\Debug\Exception\FlattenException;
+use Symfony\Component\Debug\ExceptionHandler as SymfonyExceptionHandler;
+use App\Mail\ExceptionOccured;
+
 class Handler extends ExceptionHandler
 {
     /**
