576c89b02a4049f82141860c16b7bce4ab8c083e
Christine Stone <cc@battab.com>
2019-05-30 23:18:07 -0600

    Switch ordering of release notes to DESC

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -44,7 +44,7 @@ class GameController extends Controller
      */
     public function releasenotes() {
 
-        $notes = Releasenote::all();
+        $notes = Releasenote::orderBy('created_at','desc')->get();
             return view('game.releasenotes')
             ->with('notes',$notes);
     }     
