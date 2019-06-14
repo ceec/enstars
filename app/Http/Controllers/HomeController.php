83f3ddc0e3a79c2264300178491eb7e1ea76e313
Christine Stone <cc@battab.com>
2019-06-14 10:12:35 -0400

    Add baseurl to other parts of the scraper

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -538,7 +538,7 @@ class HomeController extends Controller
      */
     public function scraper() {
         //lets try just getting the images first
-        $url = 'http://stars.happyelements.co.jp/app_help/gachas/144/index.html';
+        $url = 'http://stars.happyelements.co.jp/app_help/gachas/148/index.html';
         
         $all = file_get_contents($url);
 
@@ -580,7 +580,7 @@ class HomeController extends Controller
 
         //get the icon images
         $crawler->filter('div.thumb-nav a img')->each(function($icons) {
-            $baseurl = 'http://stars.happyelements.co.jp/app_help/gachas/144';
+            $baseurl = 'http://stars.happyelements.co.jp/app_help/gachas/148';
 
             $image = $icons->attr('src');
             $image = ltrim($image,'.');
@@ -590,6 +590,8 @@ class HomeController extends Controller
 
         // Get the latest post in this category and display the titles
         $cards = $crawler->filter('dl.cardDetail')->each(function ($node) {
+
+            $baseurl = 'http://stars.happyelements.co.jp/app_help/gachas/148';
             //create a counter to match with the images array
             //images
             //looks like in the source itself the image is indeed just /
@@ -682,11 +684,13 @@ class HomeController extends Controller
         // print_r($cards);
         // print '</pre>';
 
+        $baseurl = 'http://stars.happyelements.co.jp/app_help/gachas/148';
+
         //add in card images
         foreach($cards as $key => $card) {
             //print $images[$key];
-            print '<img src="http://stars.happyelements.co.jp/app_help/gachas/144/images/cd_'.$images[$key].'_n.png">';
-            print '<img src="http://stars.happyelements.co.jp/app_help/gachas/144/images/cd_'.$images[$key].'_e.png">';
+            print '<img src="'.$baseurl.'/images/cd_'.$images[$key].'_n.png">';
+            print '<img src="'.$baseurl.'/images/cd_'.$images[$key].'_e.png">';
             print '<pre>';
             print_r($card);
             print '</pre>';
