cbee5af051d92e7e7f279038044f839b14eafb09
Christine Stone <cc@battab.com>
2020-01-08 21:27:59 -0700

    switch collection to eden

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -11,8 +11,8 @@ enstars.info - Information and Translations for Ensemble Stars!
                 <div class="col-md-4">
                     <h3>Current Event</h3>
                     <!-- Unit Collection hard coded -->
-                    <h4>Unit Collection Knights</h4>
-                    <a href="/unitcollection/knights"><img class="img-responsive" src="/images/collections/8.png" alt="Unit Collection Knights"></a><br>
+                    <h4>Unit Collection Eden</h4>
+                    <a href="/unitcollection/eden"><img class="img-responsive" src="/images/collections/9.png" alt="Unit Collection Eden"></a><br>
 
                     @foreach ($current_event as $event)
                         <h4>{{$event->name_e}}</h4>
