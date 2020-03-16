dc84c44a6f10199b341a3381e18f7e49d9a9dc64
Christine Stone <cc@battab.com>
2020-03-15 20:58:25 -0600

    Remove old skills from selection

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -42,7 +42,7 @@ class CardController extends Controller
             $boys = Boy::orderBy('first_name','ASC')->pluck('first_name','id');
 
 
-            $lesson_skills = Skill::where('skilltype_id','=','2')->orderBy('category','ASC')->orderBy('type','ASC')->orderBy('size','ASC')->pluck('english_description','id');
+            $lesson_skills = Skill::where('skilltype_id','=','2')->orderBy('category','ASC')->orderBy('type','ASC')->orderBy('size','ASC')->where('id','>',92)->pluck('english_description','id');
             $dorifes_skills = Skill::where('skilltype_id','=','1')->orderBy('category','ASC')->orderBy('type','ASC')->orderBy('size','ASC')->orderBy('amount','ASC')->pluck('english_description','id');
 
             return view('home.cardAdd')
