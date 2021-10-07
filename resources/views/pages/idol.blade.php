907328c23268c7f923f1e43abbc16ab79dad9183
starstarstar <67977772+the0star@users.noreply.github.com>
2021-10-07 14:32:11 -0400

    adjust indentation

--- resources/views/pages/idol.blade.php
+++ resources/views/pages/idol.blade.php
@@ -1,73 +1,75 @@
 @extends('layouts.layout')
 
 @section('title')
-@parent
-{{$boy->english_name}} | enstars.info
+    @parent
+    {{$boy->english_name}} | enstars.info
 @stop
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
 
-            <h1>{{$boy->first_name}} {{$boy->last_name}}</h1>
+                <h1>{{$boy->first_name}} {{$boy->last_name}}</h1>
                 <div id="cards" class="row">
-                    <?php $x=1; ?>
+                    <?php $x = 1; ?>
                     @foreach($basic_cards as $card)
                         {{ $card->display() }}
                         <?php
-                            if ($x%4==0) {
-?>
-                            </div>
-                            <div class="row">
-<?php                            
-                            }
-                            $x++;
+                        if ($x % 4 == 0) {
                         ?>
+                </div>
+                <div class="row">
+                    <?php
+                    }
+                    $x++;
+                    ?>
                     @endforeach
-                </div>            
+                </div>
                 <hr>
                 <div id="cards" class="row">
-                    <?php $x=1; ?>
+                    <?php $x = 1; ?>
                     @foreach($cards as $card)
                         {{ $card->display() }}
                         <?php
-                            if ($x%4==0) {
-?>
-                            </div>
-                            <div class="row">
-<?php                            
-                            }
-                            $x++;
+                        if ($x % 4 == 0) {
                         ?>
+                </div>
+                <div class="row">
+                    <?php
+                    }
+                    $x++;
+                    ?>
                     @endforeach
                 </div>
 
 
+            </div>
         </div>
-    </div>
-    <h2>Information</h2>
-    <div class="row">
-        <div class="col-md-6">
-        @if ($boy->unit_id > 0)
-        <h3>Unit: <a href="/unit/{{$boy->unit->url}}">{{$boy->unit->name}}</a></h3>
-        @endif
-        @if ($boy->class_id > 0)
-        <h3>Class: <a href="/class/{{$boy->classroom->class}}">{{$boy->classroom->class}}</a></h3>
-        @endif
-        <!--<h3>Club: <a href="">{{$boy->unit_id}}</a></h3>-->
+        <h2>Information</h2>
+        <div class="row">
+            <div class="col-md-6">
+                @if ($boy->unit_id > 0)
+                    <h3>Unit: <a href="/unit/{{$boy->unit->url}}">{{$boy->unit->name}}</a></h3>
+                @endif
+                @if ($boy->class_id > 0)
+                    <h3>Class: <a href="/class/{{$boy->classroom->class}}">{{$boy->classroom->class}}</a></h3>
+            @endif
+            <!--<h3>Club: <a href="">{{$boy->unit_id}}</a></h3>-->
 
-        </div>
-        <div class="col-md-6">
+            </div>
+            <div class="col-md-6">
 
+            </div>
         </div>
-    </div>
-    <h2>Stories</h2>
+        <h2>Stories</h2>
         @foreach($chapters as $chapter)
             @if($chapter->chapter->complete == 1)
-            <a href="/story/{{$chapter->chapter->story->id}}">{{$chapter->chapter->story->name_e}}</a> - <a href="/story/{{$chapter->chapter->story->id}}/{{$chapter->chapter->chapter}}">{{$chapter->chapter->name_e}}</a><br>
+                <a href="/story/{{$chapter->chapter->story->id}}">{{$chapter->chapter->story->name_e}}</a> - <a
+                        href="/story/{{$chapter->chapter->story->id}}/{{$chapter->chapter->chapter}}">{{$chapter->chapter->name_e}}</a>
+                <br>
             @endif
         @endforeach
-     <script src="/js/enstars.js"></script>   
-</div>
+        <script src="/js/enstars.js"></script>
+    </div>
 @endsection
