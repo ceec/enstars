6421feb4ef9e0f178c842ba15d3ba2906bcd911c
Christine Stone <cc@battab.com>
2021-11-22 19:53:50 -0700

    Worst way to force a css refresh

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -21,7 +21,7 @@
 
     <!-- Styles -->
     <link href="/css/bootstrapyeti.min.css" rel="stylesheet">
-    <link href="/css/enstars2.css" rel="stylesheet">
+    <link href="/css/enstars3.css" rel="stylesheet">
     <link href="/css/boy.css" rel="stylesheet">
 
     <!-- JS -->
